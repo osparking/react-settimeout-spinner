3428ee2c010d46380a65e8234234dbf8b9dbb2b8
osparking <jbpark03@gmail.com>
2024-12-02 13:31:44 +0900

    App uses setTimeout method during loading...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// Filename - App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn 리액트
-        </a>
-      </header>
-    </div>
-  );
-}
+import { useEffect, useState } from "react";
+import "./App.css";
 
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData("환. 범이비누 .영");
+      setIsLoading(false);
+    }, 1500);
+  }, []);
+
+  if (isLoading) {
+    return <div className="spinner">Loading.....</div>;
+  } else {
+    return <div className="container">{data}</div>;
+  }
+};
 export default App;
